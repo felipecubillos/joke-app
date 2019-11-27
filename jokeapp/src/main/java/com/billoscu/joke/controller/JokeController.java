071306dc903e1e204c86e528be29c75484b84d08
Felipe Cubillos <felipe.cubillos@outlook.com>
2019-11-26 20:57:04 -0500

    another way to implement autowired

--- jokeapp/src/main/java/com/billoscu/joke/controller/JokeController.java
+++ jokeapp/src/main/java/com/billoscu/joke/controller/JokeController.java
@@ -6,20 +6,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.billoscu.joke.services.JokeService;
+import com.billoscu.joke.services.JokeServiceImpl;
 
 @Controller
 public class JokeController {
 
-	private JokeService jokeService;
+	//private JokeService jokeService;
+	@Autowired
+	private JokeServiceImpl jokeServiceImpl;
 
+	/*
 	@Autowired
 	public JokeController(JokeService jokeService) {
 	this.jokeService = jokeService;	
-	}
+	}*/
+	
+	public JokeController(JokeServiceImpl jokeServiceImpl) {
+		this.jokeServiceImpl = jokeServiceImpl;	
+		}
 	
 	@RequestMapping({"/",""})
 	public String showJokes(Model model) {
-		model.addAttribute("joke",jokeService.getJoke());
+		model.addAttribute("joke",jokeServiceImpl.getJoke());
 		return "chucknorris"; // name of thymeleaf view chucknorrys.html
 	}
 
